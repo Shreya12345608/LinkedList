608061b3a078f2609332f6d3e2a638aaa6e98733
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-13 14:03:50 +0530

    [Shreya] Add: Create a simple Linkedlist 56,30,70

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        internal Node head;
+        /// <summary>
+        /// method for add element
+        /// </summary>
+        /// <param name="data">value send to th user</param>
+        internal void Add(int data)
+        {
+            //creating a node with given data
+            Node node = new Node(data);
+            //if head is null means the list is empty
+            ////the first  node is head
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                //esle add the element to the end
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            //print and inserted into the linked list
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        // for display
+        internal void Display()
+        {
+            Node temp = this.head;
+            ////if head is null means the list is empty
+            if (temp == null)
+            {
+                //print linked list is empty
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            // traverse and print data from head to last
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        // initialize vriable int data
+        public int data;
+        // Node next
+        public Node next;
+        /// <summary>
+        /// Constructor to initilize data variables
+        /// </summary>
+        /// <param name="data">data that create node</param> 
+        public Node(int data)
+        {
+            this.data = data;
+        }// end:: public  Node(int data)
+    } //    public class Node
+}
